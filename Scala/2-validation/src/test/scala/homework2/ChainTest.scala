fc568f589341f7a2e279b20a20bf2da647a042ca
Yana Zdravkova <yana.zdravkovaa@gmail.com>
2019-05-16 23:41:40 +0300

    HW2 Add Chain tests

--- Scala/2-validation/src/test/scala/homework2/ChainTest.scala
+++ Scala/2-validation/src/test/scala/homework2/ChainTest.scala
@@ -7,9 +7,24 @@ class ChainTest extends FlatSpec with Matchers {
     (Chain(1, 2) ++ Chain(3, 4)) shouldEqual Chain(1, 2, 3, 4)
   }
   
-  "isEmpty" should "recognize as non empty chain" in {
+  "isEmpty" should "recognize as NON EMPTY chain" in {
     (Chain(1, 2).isEmpty shouldBe false)
   }
   
+  "equals" should "recognize two equal chains" in {
+    (Chain(1, 2, 3).equals(Chain(1, 2, 3)) shouldBe true)
+  }
+  
+  "equals" should "recognize two NOT equal chains" in {
+    (Chain(1, 2, 5).equals(Chain(4)) shouldBe false)
+  }
+  
+  
+  "+:" should "add one front element" in {
+    (Chain(1, 2).+:(3) shouldBe Chain(3, 1, 2)) 
+  }
+  
+   ":+" should "add one back element" in {
+    (Chain(1, 2).:+(3) shouldBe Chain(1, 2, 3)) 
   }
 }
